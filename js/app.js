9efde003c38eb25ed17f10ac6c5698b194931e77
Ramses Llamosas <gonzalezllamosas07@gmail.com>
2024-03-05 18:23:18 -0600

    Primera versión del proyecto

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){
-            
+
+
+    // Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
+
+
+    // Asignar eventos
+    inputEmail.addEventListener('blur', validar);
+    inputAsunto.addEventListener('blur', validar);
+    inputMensaje.addEventListener('blur', validar);
+
+    function validar(e){
+        if(e.target.value.trim() === ''){
+            mostrarAlerta();
+        }else{
+            console.log('Si hay algo');
+        }
+
+    }
+
+    function mostrarAlerta(){
+        // Generar alerta en HTML
+        const error = document.createElement('P'); 
+        error.textContent = 'Hubo un error';
+
+        // Inyectar el error al formulario
+        
+    }
 });
